329751aea7436364a557341fecad69f727622012
Amparo Cabezuelo Sepulveda <amcasep@gmail.com>
2024-04-28 21:42:57 +0200

    implemented toggle button to change the theme dark/light

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,17 +1,52 @@
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+import { FaRegCircle } from "react-icons/fa";
+
+import { useState, useEffect } from "react";
 
 const NavBar = () => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+    const [modeText, setIsModeText] = useState("light on")
+
     const handleLinkClick = (event, targetId) => {
         // Close the dropdown menu
         const checkbox = document.getElementById('active');
         checkbox.checked = false;
     };
 
+    useEffect(() => {
+        // Apply dark mode styles
+        if (isDarkMode) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    }, [isDarkMode]);
+
+    const handleModeButton = () => {
+        toggleDarkMode();
+        toggleText()
+    };
+
+    const toggleDarkMode = () => {
+        setIsDarkMode(!isDarkMode);
+    };
+    
+    const toggleText = () => {
+        if (modeText === 'light on') {
+            setIsModeText('light off');
+        } else {
+            setIsModeText('light on');
+        }
+    };
+
     return (
         <>
             <nav className="navBar">
-
+                <div className="toggleMode">
+                    <FaRegCircle onClick={handleModeButton} />
+                    <p>{modeText}</p>
+                </div>
                 <input type="checkbox" id="active" />
                 <label for="active" className="menu-btn"><span></span></label>
                 {/* <label for="active" className="close"></label> */}
